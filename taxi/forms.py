a2ed865baffae0edcc29fd3959487db609481ad7
vadym <vadym@ohmanalytics.com>
2025-04-07 21:14:06 +0300

    refactored forms

--- taxi/forms.py
+++ taxi/forms.py
@@ -5,22 +5,23 @@ from django.core.validators import MinLengthValidator, RegexValidator
 
 from taxi.models import Driver, Car
 
-LICENSE_NUMBER_LENGTH = 8
 LICENSE_NUMBER_REGEX = r"^[A-Z]{3}\d{5}$"
 LICENSE_NUMBER_ERROR_MESSAGE = ("License must be 3 uppercase letters "
                                 "followed by 5 digits (e.g., ABC12345).")
 
-
-class DriverCreationForm(UserCreationForm):
-    license_number = forms.CharField(
-        min_length=LICENSE_NUMBER_LENGTH,
-        max_length=LICENSE_NUMBER_LENGTH,
-        required=True,
-        validators=[RegexValidator(
+license_number_field = forms.CharField(
+    required=True,
+    validators=[
+        RegexValidator(
             regex=LICENSE_NUMBER_REGEX,
             message=LICENSE_NUMBER_ERROR_MESSAGE,
-        )],
-    )
+        )
+    ],
+)
+
+
+class DriverCreationForm(UserCreationForm):
+    license_number = license_number_field
 
     class Meta(UserCreationForm.Meta):
         model = Driver
@@ -29,17 +30,7 @@ class DriverCreationForm(UserCreationForm):
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
-    license_number = forms.CharField(
-        min_length=LICENSE_NUMBER_LENGTH,
-        max_length=LICENSE_NUMBER_LENGTH,
-        required=True,
-        validators=[
-            RegexValidator(
-                regex=LICENSE_NUMBER_REGEX,
-                message=LICENSE_NUMBER_ERROR_MESSAGE
-            )
-        ],
-    )
+    license_number = license_number_field
 
     class Meta:
         model = Driver
